df367b83be6b439bd6ab64c4c870dcdd026b0832
Joao Pereira <joaopapereira@gmail.com>
2017-09-20 17:05:15 +0100

    Update menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,9 @@
-/**
- * Angular 2 decorators and services
- */
 import {
   Component,
   OnInit,
   ViewEncapsulation
 } from '@angular/core';
-import { AppState } from './app.service';
 
-/**
- * App Component
- * Top Level Component
- */
 @Component({
   selector: 'app',
   encapsulation: ViewEncapsulation.None,
@@ -19,47 +11,29 @@ import { AppState } from './app.service';
     './app.component.css'
   ],
   template: `
-    <nav>
-      <a [routerLink]=" ['./'] "
-        routerLinkActive="active" [routerLinkActiveOptions]= "{exact: true}">
-        Index
-      </a>
-      <a [routerLink]=" ['./home'] "
-        routerLinkActive="active" [routerLinkActiveOptions]= "{exact: true}">
-        Home
-      </a>
-      <a [routerLink]=" ['./about'] "
-        routerLinkActive="active" [routerLinkActiveOptions]= "{exact: true}">
-        About
-      </a>
-      <a [routerLink]=" ['./idea'] "
-        routerLinkActive="active" [routerLinkActiveOptions]= "{exact: true}">
-        Ideas
-      </a>
-    </nav>
+    <div>
+      <nav>
+        <a [routerLink]=" ['./home'] "
+          routerLinkActive="active" [routerLinkActiveOptions]= "{exact: true}">
+          Home
+        </a>
+        <a [routerLink]=" ['./about'] "
+          routerLinkActive="active" [routerLinkActiveOptions]= "{exact: true}">
+          About
+        </a>
+        <a [routerLink]=" ['./idea'] "
+          routerLinkActive="active" [routerLinkActiveOptions]= "{exact: true}">
+          Repository of Ideas
+        </a>
+      </nav>
+    </div>
 
-    <main>
-      <router-outlet></router-outlet>
-    </main>
-
-    <pre class="app-state">this.appState.state = {{ appState.state | json }}</pre>
+    <div>
+      <main>
+        <router-outlet></router-outlet>
+      </main>
+    </div>
   `
 })
-export class AppComponent implements OnInit {
-  constructor(
-    public appState: AppState
-  ) {}
-
-  public ngOnInit() {
-    console.log('Initial App State', this.appState.state);
-  }
-
+export class AppComponent {
 }
-
-/**
- * Please review the https://github.com/AngularClass/angular2-examples/ repo for
- * more angular app examples that you may copy/paste
- * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
- * For help or questions please contact us at @AngularClass on twitter
- * or our chat on Slack at https://AngularClass.com/slack-join
- */
